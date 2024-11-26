2684340957ad58dafaecfa5ad2d0301f4a8d8191
vifff <vikasalex1996@gmail.com>
2024-11-26 14:37:56 +0530

    Add to Cart

---
+++ src/services/cartServices.js
@@ -0,0 +1,25 @@
+import { API_CART } from '../constants/ApiConstants';
+import { toast } from 'react-toastify';
+import useAxiosInstance from '../api/AxiosInstance';
+
+const useCartService = () => {
+    const AxiosInstance = useAxiosInstance();
+
+    const addProductToCart = async (productId, userId) => {
+        try {
+            const quantity = 1;
+            const payload = { userId, productId, quantity };
+            let result = await AxiosInstance.post(`${API_CART}`, payload);
+            if (result) {
+                toast.success('Product Added Successfully!');
+            }
+        } catch (error) {
+            console.error('Error Fetching data ' + error);
+            toast.error('Failed to Add  to Cart. Please try again');
+        }
+    }
+
+    return { addProductToCart };
+}
+
+export default useCartService;
